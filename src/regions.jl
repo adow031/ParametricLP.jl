95b9bb78e6ec25951cce75e16d0990996418f7ad
Tony Downward <a.downward@auckland.ac.nz>
2023-01-08 23:43:47 +1300

    Fixed docstring

--- src/regions.jl
+++ src/regions.jl
@@ -159,17 +159,17 @@ end
         ϵ = 0.01,
     )
 
-    This function finds all the optimal bases and their corresponding regions in terms of two `parameters` in a linear programming `model`.
-    
-    ### Required arguments
-    `model` is a `JuMP.Model` linear programme, defined in a way where two of the right-hand side values are replaced by variables.
-    
-    `parameters` is a vector of two variables that appear on the right-hand side of the two constraints that we wish to explore parametrically.
+This function finds all the optimal bases and their corresponding regions in terms of two `parameters` in a linear programming `model`.
 
-    `box` is a Tuple of Tuples, this defines the minimum and maximum value for each of the `parameters` described above.
+### Required arguments
+`model` is a `JuMP.Model` linear programme, defined in a way where two of the right-hand side values are replaced by variables.
 
-    ### Optional arguments
-    `ϵ` is a tolerance value used when ensuring that we find an adjacent basis.
+`parameters` is a vector of two variables that appear on the right-hand side of the two constraints that we wish to explore parametrically.
+
+`box` is a Tuple of Tuples, this defines the minimum and maximum value for each of the `parameters` described above.
+
+### Optional arguments
+`ϵ` is a tolerance value used when ensuring that we find an adjacent basis.
 """
 function find_regions(
     model::JuMP.Model,
